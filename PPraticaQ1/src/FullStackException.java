8b2c19b1abcb829084d288bf006ab9aaefe6262f
jpfigueiredo <102195109+jpfigueiredo@users.noreply.github.com>
2022-11-29 22:57:22 -0300

    Add files via upload

---
+++ PPraticaQ1/src/FullStackException.java
@@ -0,0 +1,12 @@
+public class FullStackException extends Exception {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FullStackException(String string) {
+        System.out.println(string);
+    }
+
+}
